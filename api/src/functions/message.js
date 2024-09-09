a20774de2fe00b5da400d88a4c816e34f0d27d5b
John Doe <john.doe@contoso.com>
2024-09-09 09:56:14 -0500

    Refactor API fetch logic in message.js

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -6,7 +6,7 @@ app.http('message', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'world from managed function';
 
         return { body: `Hello, ${name}!` };
     }
